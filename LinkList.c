94d55116bdce3e711d8acacbf1dde502f399419e
yyhmoment <yyh2022moment@163.com>
2022-09-24 11:49:29 +0800

    LinkList second commit OK

--- LinkList.c
+++ LinkList.c
@@ -29,7 +29,7 @@ void CreatListH(LinkList *head, int n)
   for (i = 0; i < n; i++)
   {
     s = (LinkList *)malloc(sizeof(LinkList)); /*malloc分配所需的内存空间，并返回一个指向它的指针，*/
-    scanf("%d", &s->data);                   /*其中（Linklist *）就是要malloc返回指向这种类型的一个指针*/
+    scanf("%d", &s->data);                    /*其中（Linklist *）就是要malloc返回指向这种类型的一个指针*/
     s->next = head->next;
     head->next = s;
   }
@@ -54,25 +54,26 @@ void CreateListL(LinkList *head, int n)
   printf("尾插法建立单链表成功！");
 }
 
-void InsList(LinkList* head, int i, DataType x)
-{	/*按位置插入元素函数*/
-    int j = 0;
-    LinkList* p, * s;
-    p = head;
-    while (p->next != NULL && j < i - 1)	/*定位插入点*/
-    {
-        p = p->next;
-        j++;
-    }
-    if (p != NULL)		/*P不为空则将新结点插到p后*/
-    {
-        s = (LinkList*)malloc(sizeof(LinkList));	/*生成新结点*/
-        s->data = x;								/*将数据x放入新结点的数据域*/
-        s->next = p->next;							/*将新结点s的指针域与p结点后面元素相连*/
-        p->next = s;								/*将p与新结点s链接*/
-        printf("插入元素成功！");
-    }
-    else printf("插入元素失败！");
+void InsList(LinkList *head, int i, DataType x)
+{ /*按位置插入元素函数*/
+  int j = 0;
+  LinkList *p, *s;
+  p = head;
+  while (p->next != NULL && j < i - 1) /*定位插入点*/
+  {
+    p = p->next;
+    j++;
+  }
+  if (p != NULL) /*P不为空则将新结点插到p后*/
+  {
+    s = (LinkList *)malloc(sizeof(LinkList)); /*生成新结点*/
+    s->data = x;                              /*将数据x放入新结点的数据域*/
+    s->next = p->next;                        /*将新结点s的指针域与p结点后面元素相连*/
+    p->next = s;                              /*将p与新结点s链接*/
+    printf("插入元素成功！");
+  }
+  else
+    printf("插入元素失败！");
 }
 
 /*3.按位置删除链表中元素*/
@@ -89,14 +90,15 @@ void DelList(LinkList *head, int i)
   }
   if (p->next != NULL && j == i - 1)
   {
-      s = p->next;/*存储被删除结点的地址*/
-      x = s->data; /*存储被删结点的data值*/
-      p->next = s->next;
-      free(s);
-      printf("删除第%d位上的元素%d成功！",i,x);
+    s = p->next; /*存储被删除结点的地址*/
+    x = s->data; /*存储被删结点的data值*/
+    p->next = s->next;
+    free(s);
+    printf("删除第%d位上的元素%d成功！", i, x);
   }
-  else {
-      printf("删除结点位置错误，删除失败！");
+  else
+  {
+    printf("删除结点位置错误，删除失败！");
   }
 }
 
@@ -189,76 +191,118 @@ void MenuLine()
 
 int main()
 {
-    LinkList* head;
-    DataType x;
-    int i, n;
-    char ch1, ch2, a;
-    ch1 = 'y';
-    while (ch1 == 'y'|| ch1 == 'Y') {
-        MenuLine();
-        scanf("%c",&ch2);
-        getchar();/*防止数据类型不一致*/
-        switch (ch2)
-        {
-        case '1':/*用尾插法创建链表*/
-            head = InitList();
-            printf("请输入要建立数据表的长度：");
-            scanf("%d", &n);
-            CreateListL(head, n);/*这里使用尾插法来建立链表，如果要使用头插法，就改为CreatListH(L)*/
-            printf("建立后的线性表为：\n");
-            DispList(head);
-            break;
-        case '2':/*按位置插入元素*/
-            printf("请输入要插入的元素位置：");
-            scanf("%d", &i);
-            getchar();
-            printf("请输入要插入的元素值：");
-            scanf("%d", &x);
-            InsList(head, i, x);
-            printf("插入元素%d后的线性表为：\n", x);
-            DispList(head);
-            break;
-        case '3':/*删除元素*/
-            printf("请输入要删除的元素位置：");
-            scanf("%d", &i);
-            DelList(head, i);
-            printf("删除第%d位的元素后的线性表为：\n");
-            DispList(head);
-            break;
-        case '4':/*按位置查找元素*/
-            printf("请输入查找的元素位置（大于等于1的整数）：");
-            scanf("%d", &i);
-            SearchList(head, i);
-            break;
-        case '5':/*安置查找元素位置*/
-            printf("请输入查找的整数：");
-            scanf("%d", &x);
-            Locate(head, x);
-            break;
-        case '6':/*链表长度*/
-            printf("该线性表的长度为%d！", LengthList(head));
-            break;
-        case'7':
-            printf("输出链表：");
-            DispList(head);
-            break;
-        case '0':/*返回操作*/
-            ch1 = 'n';
-            break;
-        default:
-            printf("输入有误，请输入0-9进行选择！");
-        }
-        if (ch2 != '0') 
-        {
-            printf("\n按回车键继续，按任意键返回主菜单！\n");
-            a = getchar();
-            if(a != '\xA') {
-                getchar();
-                ch1 = 'n'; /*控制while循环*/
-            }
-        }
+  LinkList *head;
+  DataType x;
+  int i, n;
+  char ch1, ch2, a;
+  ch1 = 'y';
+  head = InitList();
+  while (ch1 == 'y' || ch1 == 'Y')
+  {
+    MenuLine();
+    scanf("%c", &ch2);
+    getchar(); /*防止数据类型不一致*/
+    switch (ch2)
+    {
+    case '1': /*用尾插法创建链表*/
+      // head = InitList();
+      printf("请输入要建立数据表的长度：");
+      scanf("%d", &n);
+      CreateListL(head, n); /*这里使用尾插法来建立链表，如果要使用头插法，就改为CreatListH(L)*/
+      printf("建立后的线性表为：\n");
+      DispList(head);
+      break;
+    case '2':                                 /*按位置插入元素*/
+      if (head->next == NULL || head == NULL) /*判定当带头结点的指针域或不带头结点的指针域为空时，*/
+      {                                       /*即表示此时链表为空，此时就提示用户先建表再进行此操作*/
+        printf("请建链表后再进行插入操作!\n");
+        continue; /*跳过此次操作*/
+      }
+      else
+      {
+        printf("请输入要插入的元素位置：");
+        scanf("%d", &i);
+        getchar();
+        printf("请输入要插入的元素值：");
+        scanf("%d", &x);
+        InsList(head, i, x);
+        printf("插入元素%d后的线性表为：\n", x);
+        DispList(head);
+      }
+      break;
+    case '3': /*删除元素*/
+      if (head->next == NULL || head == NULL)
+      {
+        printf("请建链表后再进行删除操作!\n");
+        continue;
+      }
+      else
+      {
+        printf("请输入要删除的元素位置：");
+        scanf("%d", &i);
+        DelList(head, i);
+        printf("删除第%d位的元素后的线性表为：\n");
+        DispList(head);
+      }
+      break;
+    case '4': /*按位置查找元素*/
+      if (head->next == NULL || head == NULL)
+      {
+        printf("请建链表后再进行按位置查找元素操作!\n");
+        continue;
+      }
+      else
+      {
+        printf("请输入查找的元素位置（大于等于1的整数）：");
+        scanf("%d", &i);
+        SearchList(head, i);
+      }
+      break;
+    case '5': /*按值查找元素位置*/
+      if (head->next == NULL || head == NULL)
+      {
+        printf("请建链表后再进行按值查找操作!\n");
+        continue;
+      }
+      else
+      {
+        printf("请输入查找的整数：");
+        scanf("%d", &x);
+        Locate(head, x);
+      }
+      break;
+    case '6': /*链表长度*/
+      printf("该线性表的长度为%d！", LengthList(head));
+      break;
+    case '7':
+      if (head->next == NULL || head == NULL)
+      {
+        printf("当前为空表，请建表后再进行此操作！");
+        continue;
+      }
+      else
+      {
+        printf("输出链表：");
+        DispList(head);
+      }
+      break;
+    case '0': /*返回操作*/
+      ch1 = 'n';
+      break;
+    default:
+      printf("输入有误，请输入0-9进行选择！");
     }
+    if (ch2 != '0')
+    {
+      printf("\n按回车键继续，按任意键返回主菜单！\n");
+      a = getchar();
+      if (a != '\xA')
+      {
+        getchar();
+        ch1 = 'n'; /*控制while循环*/
+      }
+    }
+  }
 
- 
   return 0;
 }
