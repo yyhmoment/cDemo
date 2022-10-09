9842a48fae722377030d6519f19d61679c709404
yyhmoment <yyh2022moment@163.com>
2022-10-09 13:18:53 +0800

    String second commit

--- String.c
+++ String.c
@@ -15,19 +15,19 @@ int StrLength(String *S)
   while (S->ch[i] != '\0')
   {
     i++;
-    S->Len = i;
-    return (S->Len); //返回Len值，即串长度
   }
+  S->Len = i;
+  return (S->Len); //返回Len值，即串长度
 }
 
-/*建立一个新串*/
+/*1.建立一个新串*/
 void CreatStr(String *S)
 {
   gets(S->ch);
   S->Len = StrLength(S);
 }
 
-/*求字串函数*/
+/*2.求字串函数*/
 int SubString(String *S, String *Sub, int pos, int len)
 {
   int j;
@@ -42,41 +42,39 @@ int SubString(String *S, String *Sub, int pos, int len)
     for (j = 0; j < len; j++)
     {
       Sub->ch[j] = S->ch[pos + j - 1];
-      Sub->ch[j] = '\0';
-      Sub->Len = len;
-      return 1;
     }
+    Sub->ch[j] = '\0';
+    Sub->Len = len;
+    return 1;
   }
 }
 
-/*在串s中删除从指定位置i开始连续的j个字符*/
+/*3.在串s中删除从指定位置i开始连续的j个字符*/
 int StrDelete(String *S, int i, int l)
 {
   int k;
-  if (i + 1 - 1 > S->Len)
+  if (i + l - 1 > S->Len)
   {
     printf("所要删除的字串超界！");
     return 0;
   }
   else
   {
-    for (k = i + 1 - 1; k < S->Len; k++, i++)
-    {
+    for (k = i + l - 1; k < S->Len; k++, i++)
       S->ch[i - 1] = S->ch[k];
-      S->Len = S->Len - 1;
-      S->ch[S->Len] = '\0';
-      return 1;
-    }
+    S->Len = S->Len - l;
+    S->ch[S->Len] = '\0';
+    return 1;
   }
 }
 
-/*在串s中插入字串s1函数*/
+/*4.在串s中插入字串s1函数*/
 int StrInsert(String *S, String *S1, int i)
-{
+{ /*在串s中插入子串s1函数*/
   int k;
   if (i > S->Len + 1)
   {
-    printf("插入位置错误！");
+    printf("插入位置有误！");
     return 0;
   }
   else if (S->Len + S1->Len > MAXSIZE)
@@ -86,18 +84,121 @@ int StrInsert(String *S, String *S1, int i)
   }
   else
   {
-    for (k = S->Len - 1; k > i - 1; k--)
-    {
+    for (k = S->Len - 1; k >= i - 1; k--)
       S->ch[S1->Len + k] = S->ch[k];
-      for (k = 0; k < S1->Len; k++)
-      {
-        S->ch[i + k - 1] = S1->ch[k];
-      }
-      S->Len = S->Len + S1->Len;
-      S->ch[S->Len] = '\0';
-      return 1;
+    for (k = 0; k < S1->Len; k++)
+      S->ch[i + k - 1] = S1->ch[k];
+    S->Len = S->Len + S1->Len;
+    S->ch[S->Len] = '\0';
+    return 1;
+  }
+}
+
+/*5.串的定位函数*/
+int StrIndex(String *S, String *T)
+{
+  int i = 0, j = 0, k;
+  while (i < S->Len && j < T->Len)
+  {
+    if (S->ch[i] == T->ch[j])
+    {
+      i++;
+      j++;
+    }
+    else
+    {
+      i = i - j + 1;
+      j = 0;
     }
   }
+  if (j >= T->Len)
+  {
+    k = i - T->Len + 1;
+  }
+  else
+  {
+    k = -1;
+  }
+  return k;
+}
+
+/*6.两个串判断是否相等*/
+int StrCompare(String *S1, String *S2)
+{
+  int i = 0, flag = 0;
+  while (S1->ch[i] != '\0' && S2->ch[i] != '\0')
+  {
+    if (S1->ch[i] != S2->ch[i])
+    {
+      flag = 1;
+      break;
+    }
+    else
+    {
+      i++;
+    }
+  }
+  if (flag == 0 && S1->Len == S2->Len)
+  { /*两串长度相同且所有字符都相同，两串相等*/
+    return 0;
+  }
+  else
+  {
+    return S1->ch[i] - S2->ch[i];
+  }
+}
+
+/*7.串的连接函数*/
+int StrCat(String *S, String *T)
+{
+  int i, flag;
+  /*将串T连接在串S的后面*/
+  if (S->Len + T->Len <= MAXSIZE)
+  { /*连接后串长小于MAXSIZE*/
+    for (i = S->Len; i < S->Len + T->Len; i++)
+    {
+      S->ch[i] = T->ch[i - S->Len];
+    }
+    S->ch[i] = '\0';
+    S->Len += T->Len;
+    flag = 1;
+  }
+  else if (S->Len < MAXSIZE)
+  { /*连接后串长大于MAXSIZE，但串S的长度小于MAXSIZE，即连接后串T的部分字符序列被舍弃*/
+    for (i = S->Len; i < MAXSIZE; i++)
+    {
+      S->ch[i] = T->ch[i - S->Len];
+    }
+    S->Len = MAXSIZE;
+    flag = 0;
+  }
+  else
+  { /*串S的长度等于MAXSIZE，串不被连接*/
+    flag = 0;
+  }
+  return (flag);
+}
+
+/*8.字串替换函数*/
+void StrReplace(String *S, String *T, String *V)
+{
+  int i, m, n, p, q;
+  n = S->Len;
+  m = T->Len;
+  q = V->Len;
+  p = 1;
+  do
+  {
+    i = StrIndex(S, T); /*调用定位函数得到子串T在主串S的位置*/
+    if (i != -1)
+    {                     /*当主串S中有该子串T时*/
+      StrDelete(S, i, m); //调用删除子串函数删除该子串T
+      StrInsert(S, V, i); //调用插入子串函数插入子串V
+      p = i + q;
+      S->Len = S->Len + q - m; //修改主串S的长度
+      n = S->Len;
+    }
+  } while ((p <= n - m + 1) && (i != -1));
 }
 
 /*显示菜单子函数*/
@@ -143,7 +244,8 @@ int main(int argc, char const *argv[])
       else
       {
         puts(S->ch);
-        printf("该串的长度为：%d", S->Len);
+        int num = StrLength(S);
+        printf("该串的长度为：%d", num);
       }
       break;
     case '2':
@@ -200,10 +302,81 @@ int main(int argc, char const *argv[])
       }
       break;
     case '5':
-
+      printf("请输入第一个串：");
+      CreatStr(S);
+      printf("请输入第二个串：");
+      CreatStr(S1);
+      i = StrIndex(S, S1);
+      if (i == -1)
+      {
+        printf("查找失败，主串中没有该子串！");
+      }
+      else
+      {
+        printf("查找成功，该子串在主串中第一次出现的位置为%d。", i);
+      }
+      break;
+    case '6':
+      printf("请输入第一个串：");
+      CreatStr(S);
+      printf("请输入第二个串：");
+      CreatStr(S1);
+      flag = StrCompare(S, S1);
+      if (flag > 0)
+      {
+        printf("第一个串大！");
+      }
+      else if (flag < 0)
+      {
+        printf("第二个串大！");
+      }
+      else
+      {
+        printf("两个串相等！");
+      }
+      break;
+    case '7':
+      printf("请输入第一个串：");
+      CreatStr(S);
+      printf("请输入第二个串：");
+      CreatStr(S1);
+      flag = StrCat(S, S1);
+      if (flag)
+      {
+        printf("连接以后的新串值为：");
+        puts(S->ch);
+      }
+      else
+      {
+        printf("连接失败！");
+      }
+      break;
+    case '8':
+      printf("请输入主串：");
+      CreatStr(S);
+      printf("请输入在主串中查找的字串：");
+      CreatStr(S1);
+      printf("请输入要替换的子串：");
+      CreatStr(S2);
+      StrReplace(S, S1, S2);
+      printf("替换后的主串为：");
+      puts(S->ch);
+      break;
+    case '0':
+      ch1 = 'n';
       break;
     default:
-      break;
+      printf("输入有误，请输入0~9进行选择！");
+    }
+    if (ch2 != '0')
+    {
+      printf("\n按回车键继续，按任意键返回主菜单！\n");
+      a = getchar();
+      if (a != '\xA')
+      {
+        getchar();
+        ch1 = 'n';
+      }
     }
   }
 
